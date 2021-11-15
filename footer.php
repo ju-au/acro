a6b9d52f8590a6f5e3df0e7e780b06bdd943014a
Junya <ueda.the.tallest.pilot@gmail.com>
2021-11-15 21:26:42 +1000

    Footer SP画面

--- footer.php
+++ footer.php
@@ -4,10 +4,25 @@
 
 <footer class="footer">
     <iframe class="footer__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.9660661274515!2d139.87052831553115!3d35.65320768020086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60187d5fa9c0e249%3A0xc8f6dc8071f277c2!2z44OY44Ki44O844Op44Km44Oz44K444Ki44Kv44Ot5Y2X6JGb6KW_5bqX!5e0!3m2!1sen!2sau!4v1636347754590!5m2!1sen!2sau" width="1920" height="455" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <div class="footer__buttons-sp is-shown-sp">
+        <div class="inner">
+            <a href="<?php echo esc_url(home_url('/recruit')); ?>" class="fixedLink__recruit">
+                <p><span>RECRUIT</span><br>採用情報</p>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow-white.png" alt="">
+            </a>
+            <a href="<?php echo esc_url(home_url('/reserve')); ?>" class="fixedLink__reserve">
+                <figure>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/footer/mail.png" alt="">
+                    <p>メールでの<br>ご予約</p>
+                </figure>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow-black.png" alt="">
+            </a>
+        </div>
+    </div>
     <div class="inner footer__inner">
         <div class="footer__upper">
             <div class="footer__info">
-            <a class="footer__logo" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt=""></a>
+                <a class="footer__logo" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt=""></a>
                 <p class="footer__address">
                     〒134-0085<br>
                     東京都江戸川区南葛西2-1-9<br>
@@ -25,13 +40,11 @@
         <!-- /.footer__upper -->
 
         <div class="footer__lower">
-            <p class="footer__message">
-                <?php
-                $page_id = 41;
-                $page = get_post($page_id);
-                echo $page->post_content;
-                ?>
-            </p>
+            <?php
+            $page_id = 41;
+            $page = get_post($page_id);
+            echo $page->post_content;
+            ?>
             <nav class="footer__nav">
                 <ul class="footer__navItems">
                     <li class="footer__navItem"><a href="<?php echo esc_url(home_url()); ?>">ホーム</a></li>
@@ -76,11 +89,11 @@
 
 
 <nav class="fixedLink">
-    <a href="<?php echo esc_url(home_url('/recruit')); ?>" class="fixedLink__recruit">
+    <a href="<?php echo esc_url(home_url('/recruit')); ?>" class="fixedLink__recruit is-hidden-sp">
         <p><span>RECRUIT</span><br>採用情報</p>
         <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow-white.png" alt="">
     </a>
-    <a href="<?php echo esc_url(home_url('/reserve')); ?>" class="fixedLink__reserve">
+    <a href="<?php echo esc_url(home_url('/reserve')); ?>" class="fixedLink__reserve is-hidden-sp">
         <figure>
             <img src="<?php echo get_template_directory_uri(); ?>/images/footer/mail.png" alt="">
             <p>メールでの<br>ご予約</p>
