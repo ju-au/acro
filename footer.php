3c9ffc574450eed2407089be3dd7571158f6c335
Junya <ueda.the.tallest.pilot@gmail.com>
2021-11-16 17:31:41 +1000

    Footerの地図を、Accessページで表示しないように設定

--- footer.php
+++ footer.php
@@ -3,7 +3,10 @@
 </main>
 
 <footer class="footer">
-    <iframe class="footer__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.9660661274515!2d139.87052831553115!3d35.65320768020086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60187d5fa9c0e249%3A0xc8f6dc8071f277c2!2z44OY44Ki44O844Op44Km44Oz44K444Ki44Kv44Ot5Y2X6JGb6KW_5bqX!5e0!3m2!1sen!2sau!4v1636347754590!5m2!1sen!2sau" width="1920" height="455" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    
+    <?php if (!is_page(29)) : // Accessページ以外にgoogle mapsを表示する ?>
+        <iframe class="footer__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.9660661274515!2d139.87052831553115!3d35.65320768020086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60187d5fa9c0e249%3A0xc8f6dc8071f277c2!2z44OY44Ki44O844Op44Km44Oz44K444Ki44Kv44Ot5Y2X6JGb6KW_5bqX!5e0!3m2!1sen!2sau!4v1636347754590!5m2!1sen!2sau" width="1920" height="455" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <?php endif; ?>
     <div class="footer__buttons-tab is-shown-tab">
         <div class="inner">
             <a href="<?php echo esc_url(home_url('/recruit')); ?>" class="fixedLink__recruit">
@@ -90,7 +93,9 @@
 
 <nav class="fixedLink">
     <a href="<?php echo esc_url(home_url('/recruit')); ?>" class="fixedLink__recruit is-hidden-tab">
-        <p><taban>RECRUIT</taban><br>採用情報</p>
+        <p>
+            <taban>RECRUIT</taban><br>採用情報
+        </p>
         <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow-white.png" alt="">
     </a>
     <a href="<?php echo esc_url(home_url('/reserve')); ?>" class="fixedLink__reserve is-hidden-tab">
