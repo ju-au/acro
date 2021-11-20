0a64f8cc3899c83c83feedcae3f23034287c1f3e
Junya <ueda.the.tallest.pilot@gmail.com>
2021-11-20 22:37:59 +1000

    Recruitページ デザイン以外

--- page-recruit.php
+++ page-recruit.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+        <?php get_template_part('./includes/top'); ?>
 
+        <?php the_content(); ?>
 
+<?php
+    endwhile;
+endif;
+?>
 <?php get_footer(); ?>
+
