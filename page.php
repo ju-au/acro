5781b22250b1c282fde12a51fcc5866467296240
Junya <ueda.the.tallest.pilot@gmail.com>
2022-03-21 15:52:07 +0930

    ギャラリー・thanksページ変更

--- page.php
+++ page.php
@@ -4,10 +4,14 @@
 
         <?php
         $top_part = "";
-        if (is_page(31)) {
-            $top_part = "./includes/page-title";
-        } else {
+        if (is_page(9) || is_page(19) || is_page(23) || is_page(33)) {
+            // メニュー・こだわり・スタイリスト・ご予約ページ
             $top_part = "./includes/top";
+        } elseif (is_page(35)) {
+            // thanksページ
+        } else {
+            // その他
+            $top_part = "./includes/page-title";
         }
         get_template_part($top_part); ?>
         <section>
